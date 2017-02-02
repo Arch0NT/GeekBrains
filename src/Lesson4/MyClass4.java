8aa0e6645268e3b16b0b3445efb7d877024557fb
Arch0NT <archanelo@bk.ru>
2017-02-03 01:32:55 +0600

    Add Lesson4

---
+++ src/Lesson4/MyClass4.java
@@ -0,0 +1,18 @@
+package Lesson4;
+
+/**
+ * Created by ArchAngelo on 02.02.2017.
+ */
+public class MyClass4 {
+    public static char map [][];
+    public static void main(String[] args) {
+
+    }
+
+    public static void init() {
+        for (c:
+             ) {
+
+        }
+    }
+}
