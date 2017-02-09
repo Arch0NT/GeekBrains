057f8ab881f3fb1639dad3c47b64a18cf9007a6d
Arch0NT <archanelo@bk.ru>
2017-02-10 01:24:25 +0600

    Add simple OOP Lesson6 with 1 Abstract + 2 extends Classes

---
+++ src/Lesson6/Animal.java
@@ -0,0 +1,7 @@
+package Lesson6;
+
+public abstract class Animal {
+    public abstract String run (int run);
+    public abstract String swim (int swim);
+    public abstract String jump (double jump);
+}
