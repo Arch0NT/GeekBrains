3ce9c4d76bc934b988754b7d51aebcf5fd19afe9
Arch0NT <archanelo@bk.ru>
2017-02-21 04:08:10 +0600

    Add Lesson8 not complete

---
+++ src/Lesson8/Controller.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+/**
+ * Created by ArchAngelo on 17.02.2017.
+ */
+public class Controller {
+}
---
+++ src/Lesson8/JavaFXXO.java
@@ -0,0 +1,18 @@
+package Lesson8;/**
+ * Created by ArchAngelo on 17.02.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXXO extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
