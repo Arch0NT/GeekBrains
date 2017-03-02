6b61e964be9b1f74e773130237ea94e5fa494f81
Arch0NT <archanelo@bk.ru>
2017-03-03 02:55:14 +0600

    Adding some code Lesson 24 HomeWork

--- src/ru/geekbrains/java/Lesson24/Launcher.java
+++ src/ru/geekbrains/java/Lesson24/Launcher.java
@@ -1,4 +1,38 @@
 package ru.geekbrains.java.Lesson24;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application implements Runnable{
+
+    public void run (){
+        System.out.println("Begin");
+        launch();
+    }
+
+    public void start (Stage primaryStage){
+        primaryStage.setTitle("JavaFX chat");
+        primaryStage.setResizable(false);
+        GridPane gridPane = new GridPane();
+        Button send = new Button("Send");
+        Label label = new Label("Main room");
+        TextArea textArea = new TextArea();
+        //textArea.setPrefColumnCount(9);
+        //textArea.setPrefRowCount(8);
+        TextField textField = new TextField("Messages write here");
+        gridPane.add(label,0,0,1,1);
+        gridPane.add(textArea,0,1,14,14);
+        gridPane.add(textField,0,15, 13,1);
+        gridPane.add(send,13,15,1,1);
+        Scene scene = new Scene(gridPane, 300,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
--- src/ru/geekbrains/java/Lesson24/MyClass24.java
+++ src/ru/geekbrains/java/Lesson24/MyClass24.java
@@ -2,6 +2,6 @@ package ru.geekbrains.java.Lesson24;
 
 public class MyClass24 {
     public static void main(String[] args) {
-
+        new Thread(new Launcher()).start();
     }
 }
